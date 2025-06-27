efba53eb0d5dada06637425a52015b7132642c4d
Eabotpro <wifileb@gmail.com>
2025-06-27 09:31:27 +0100

    sentiment_server.py

--- sentiment_server.py
+++ sentiment_server.py
@@ -64,5 +64,4 @@ if __name__ == '__main__':
     threading.Thread(target=update_sentiment, daemon=True).start()
     app.run(host="0.0.0.0", port=3000)
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3000)
+
